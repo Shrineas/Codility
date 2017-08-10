7a4c159a60d7ede7ce5ec0daeae2d1d62e1168cb
Frederik Martens <frederik.martens@sfpd.fgov.be>
2017-08-10 21:59:22 +0200

    Lesson 4 - Missing Integers
    Time optimized version, by walking the array and saving if a value exists, then returning the first not existing from the saved data.

--- src/codility/lesson4/MissingInteger.java
+++ src/codility/lesson4/MissingInteger.java
@@ -1,33 +1,31 @@
 package codility.lesson4;
 
-import java.util.Collection;
-import java.util.TreeSet;
-
 public class MissingInteger {
 
 	public int solution(int[] input) {
-		Collection<Integer> sortedNoDupes = sortAndRemoveDuplicateIntegers(input);
+        boolean[] valuesExisting = getArrayOfExistingValues(input);
 
-		return getFirstPositiveIntegerNotInArray(sortedNoDupes);
+        return getFirstPositiveValueNotExisting(valuesExisting);
 	}
 
-	private Collection<Integer> sortAndRemoveDuplicateIntegers(int[] input) {
-		Collection<Integer> sortedNoDupes = new TreeSet<>();
-		for (int value : input) {
-			sortedNoDupes.add(value);
-		}
-		return sortedNoDupes;
-	}
+    private boolean[] getArrayOfExistingValues(int[] input) {
+        boolean[] valuesExisting = new boolean[input.length + 1];
 
-	private int getFirstPositiveIntegerNotInArray(Collection<Integer> sortedNoDupes) {
-		int currentValue = 1;
-		for (Integer integer : sortedNoDupes) {
-			if (integer > 0 && integer != currentValue) {
-				return currentValue;
-			}
-			currentValue++;
-		}
-		return currentValue;
-	}
+        for (int value : input) {
+            if (value > 0 && value <= input.length) {
+                valuesExisting[value] = true;
+            }
+        }
+        return valuesExisting;
+    }
+
+    private int getFirstPositiveValueNotExisting(boolean[] valuesExisting) {
+        for (int i = 1; i < valuesExisting.length; i++) {
+            if (!valuesExisting[i]) {
+                return i;
+            }
+        }
+        return valuesExisting.length;
+    }
 
 }
